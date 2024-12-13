cb544c6983b1e804875481cc56bbf573920dc235
camron.workman <camron.workman@gmail.com>
2024-12-12 17:45:21 -0800

    Initial commit

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http'; // Import provideHttpClient
-
-bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()] // Provide HttpClient using provideHttpClient()
-}).catch(err => console.error(err));
